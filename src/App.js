562658c96197cff2400fccfb9ea6fc01921a9903
dzeilos <dzeilos@gmail.com>
2023-04-16 11:44:46 +0700

    add title

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import './App.css';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import "./App.css";
 
 import CardList from "./components/CardList/CardList";
 import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
   const [monsters, setMonsters] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const onChangeSearch = (value) => {
     setSearchValue(value.toLocaleLowerCase());
-  }
+  };
 
-  const filteredList =  monsters.filter((monster) => {
+  const filteredList = monsters.filter((monster) => {
     return monster.name.toLocaleLowerCase().includes(searchValue);
   });
 
@@ -30,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <Container>
+        <TitleContainer>Monsters Rolodex</TitleContainer>
         <SearchBox onChangeSearch={(value) => onChangeSearch(value)} />
         <CardList monsters={filteredList} />
       </Container>
@@ -43,5 +44,15 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  padding: 30px;
-`
+  padding: 10px;
+`;
+
+const TitleContainer = styled.h1`
+  color: #0ccac4;
+  margin: 15px 0 30px 0;
+  font-size: 56px;
+  transition: 0.3s;
+  :hover {
+    color: #f0f728;
+  } 
+`;
