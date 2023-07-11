1678872ce90c90b2a01640404b447f5896643af4
donny <donny.pranata3@gmail.com>
2023-07-11 15:38:28 +0700

    feat: create new MainPage component

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,19 +1,17 @@
-import { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import styled from "styled-components";
-import "./App.css";
 
-import CardList from "./components/CardList/CardList";
-import SearchBox from "./components/SearchBox/SearchBox";
+import CardList from "../CardList/CardList";
+import SearchBox from "../SearchBox/SearchBox";
+import { Monster } from "../../utils/types";
+import { getData } from "../../utils/data.utils";
 
-import { Monster } from "./utils/types";
-import { getData } from "./utils/data.utils";
-
-function App() {
+const MainPage: React.FC = () => {
   const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
   const [searchValue, setSearchValue] = useState("");
 
-  const onSearchChange = (event:ChangeEvent<HTMLInputElement>): void => {
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setSearchValue(event.target.value.toLocaleLowerCase());
   };
 
@@ -23,13 +21,12 @@ function App() {
         "https://jsonplaceholder.typicode.com/users"
       );
       setMonsters(users);
-    }
+    };
 
     fetchUsers();
 
     return () => setMonsters([]);
   }, []);
-
 
   useEffect(() => {
     const filteredList = monsters.filter((monster: Monster) => {
@@ -39,20 +36,18 @@ function App() {
   }, [monsters, searchValue]);
 
   return (
-    <div className="App">
-      <Container>
-        <TitleContainer>Monsters Rolodex</TitleContainer>
-        <SearchBox
-          onSearchChange={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </Container>
-    </div>
+    <Container>
+      <TitleContainer>Monsters Rolodex</TitleContainer>
+      <SearchBox
+        onSearchChange={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </Container>
   );
-}
+};
 
-export default App;
+export default MainPage;
 
 const Container = styled.div`
   display: flex;
@@ -68,5 +63,5 @@ const TitleContainer = styled.h1`
   transition: 0.3s;
   :hover {
     color: #f0f728;
-  } 
+  }
 `;
